df6eee433ac0969c6574436124cfdf3ef33fab01
JelenaIlic333 <madicjelena23@gmail.com>
2025-07-01 18:04:32 +0200

    Initial commit - Tailwind setup i osnova projekta

---
+++ app/components/Services.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div id="services" className='w-full px-[12%] py-10 scroll-mt-20'>
+       <h4 className='text-center mb-2 text-lg font-Ovo'>What I offer</h4>
+      <h4 className='text-center text-5xl font-Ovo'>My Services</h4>
+
+      <p className='text-center max-w-2x1 mx-auto mt-5 mb-12 font-Ovo'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+         Voluptate inventore cum quasi vitae iusto non voluptates, pariatur architecto sed quo.
+      </p>
+    </div>
+  )
+}
+
+export default Services
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+    './app/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: { extend: {
+    colors: {
+      lightHover: '#f4e1fc',
+      darkHover: '#2a004a',
+      darkTheme: '#11001F',
+    },
+    fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"]
+    },
+    boxShadow: {
+      'black' : '4px 4px 0 #000',
+      'white' : '4px 4px 0 #fff',
+    }
+  },
+ },
+  plugins: [],
+}
